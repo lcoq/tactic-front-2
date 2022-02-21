82f3069a2af615a0a5caf881a088d2cb8b6e0cdc
Louis Coquio <louis.coquio@gmail.com>
2022-02-21 08:30:35 +0100

    Fix Session mirage serializer by including only the relationship ids instead of the whole user data

--- mirage/serializers/session.js
+++ mirage/serializers/session.js
@@ -1,8 +1,5 @@
 import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
-  // TODO we should not include user by default here and instead use `serializeIds: 'always'`
-  // option but it does not work with JSONAPISerializer for now :
-  // https://github.com/miragejs/ember-cli-mirage/issues/2365
-  include: ['user'],
+  alwaysIncludeLinkageData: true
 });

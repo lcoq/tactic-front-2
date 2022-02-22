e529af5c242b2bb3ee62a889175a960207917afa
Louis Coquio <louis.coquio@gmail.com>
2022-02-22 09:34:56 +0100

    Avoid user summary to blink on reload

--- app/components/user-summary.js
+++ app/components/user-summary.js
@@ -5,13 +5,29 @@ import { bool } from '@ember/object/computed';
 export default class UserSummaryComponent extends Component {
   @computed('args.weekEntries')
   get weekEntriesDuration() {
-    return this._computeDuration(this.args.weekEntries);
+    const entries = this.args.weekEntries;
+    if (!entries && this._lastComputedWeekEntriesDuration) {
+      return this._lastComputedWeekEntriesDuration;
+    }
+    const duration = this._computeDuration(entries);
+    this._lastComputedWeekEntriesDuration = duration;
+    return duration;
   }
 
   @computed('args.monthEntries')
   get monthEntriesDuration() {
-    return this._computeDuration(this.args.monthEntries);
+    const entries = this.args.monthEntries;
+    if (!entries && this._lastComputedMonthEntriesDuration) {
+      return this._lastComputedMonthEntriesDuration;
+    }
+    const duration = this._computeDuration(entries);
+    this._lastComputedMonthEntriesDuration = duration;
+    return duration;
   }
+
+  _lastComputedWeekEntriesDuration = null;
+  _lastComputedMonthEntriesDuration = null;
+
 
   _computeDuration(entries) {
     return (entries ?? []).reduce(function (sum, entry) {

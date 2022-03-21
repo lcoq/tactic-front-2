60ad22e23d36c913fb924d7a0b8ec29c6331a6a4
Louis Coquio <louis.coquio@gmail.com>
2022-03-21 18:38:56 +0100

    Format user notification messages

--- app/components/notification.js
+++ app/components/notification.js
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { htmlSafe } from '@ember/template';
 
 export default class NotificationComponent extends Component {
   get notif() {
@@ -25,8 +26,19 @@ export default class NotificationComponent extends Component {
     return classes.join(' ');
   }
 
+  get formattedMessage() {
+    const formatted = this.notif.message
+          .replace(/\r?\n/g, '<br>')
+          .replace(this._urlRegexp, '<a class="notif__description-link" href="$1" target="_blank">$1</a>');
+    return htmlSafe(formatted);
+  }
+
   @action async onResourceClick() {
     const resource = await this.notif.resource;
     this.args.onResourceClick(resource);
+  }
+
+  get _urlRegexp() {
+    return /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/g;
   }
 }
